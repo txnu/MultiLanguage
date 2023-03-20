92c45bb884c7e472fab28e9b36268f6487ecdea2
TANU WIJAYA <tanuxvi2@gmail.com>
2023-03-20 19:53:43 +0700

    Initial commit

---
+++ app/src/main/java/com/tanuwijaya_202102261/multilanguage/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tanuwijaya_202102261.multilanguage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
